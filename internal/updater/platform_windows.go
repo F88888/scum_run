c6cfd8e0f4562e8646ab8e247e728d7b56af451c
fandengfan <fandengfan@yafex.cn>
2025-09-28 18:49:13 +0800

    fix: 更新脚本独立执行

--- internal/updater/platform_windows.go
+++ internal/updater/platform_windows.go
@@ -7,7 +7,7 @@ import "syscall"
 // getSysProcAttr 获取Windows平台特定的进程属性
 func getSysProcAttr() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{
-		HideWindow:    true,
+		HideWindow:    false, // 显示更新器窗口，让用户能看到更新进度
 		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
 	}
 }
