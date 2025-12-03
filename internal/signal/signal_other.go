661efaddc81a07d92902e31d6098a3e446fe7869
npc0-hue <github@npc0.com>
2025-12-03 17:49:50 +0800

    fix: 当控制台关闭时，应该先让 SCUM 服务器优雅关闭

--- internal/signal/signal_other.go
+++ internal/signal/signal_other.go
@@ -3,6 +3,16 @@
 
 package signal
 
+// ShutdownCallback 关闭回调函数类型
+type ShutdownCallback func()
+
+// SetShutdownCallback 设置关闭回调函数（非 Windows 平台为空操作）
+// @description: 在非 Windows 平台上通过 signal.Notify 处理
+// @param: callback ShutdownCallback 回调函数
+func SetShutdownCallback(callback ShutdownCallback) {
+	// 非 Windows 平台通过 signal.Notify 处理
+}
+
 // DisableCtrlC 禁用 Ctrl+C 信号处理（非 Windows 平台为空操作）
 // @description: 在非 Windows 平台上不需要禁用 Ctrl+C
 // @return: error 错误信息
--- internal/signal/signal_windows.go
+++ internal/signal/signal_windows.go
@@ -4,7 +4,9 @@
 package signal
 
 import (
+	"os"
 	"syscall"
+	"time"
 )
 
 var (
@@ -21,21 +23,41 @@ const (
 	CTRL_SHUTDOWN_EVENT = 6
 )
 
+// ShutdownCallback 关闭回调函数类型
+// @description: 当控制台关闭时调用，用于优雅关闭 SCUM 服务器
+type ShutdownCallback func()
+
+// shutdownCallback 保存关闭回调函数
+var shutdownCallback ShutdownCallback
+
+// SetShutdownCallback 设置关闭回调函数
+// @description: 设置当控制台关闭时调用的回调函数
+// @param: callback ShutdownCallback 回调函数
+func SetShutdownCallback(callback ShutdownCallback) {
+	shutdownCallback = callback
+}
+
 // consoleCtrlHandler 自定义控制台处理器
 // @description: 返回 1 (TRUE) 表示已处理事件，系统不应该调用默认处理器
 // @param: ctrlType uintptr 控制台事件类型
 // @return: uintptr 1 表示已处理，0 表示未处理
 func consoleCtrlHandler(ctrlType uintptr) uintptr {
-	// 忽略 CTRL+C 和 CTRL+BREAK 事件
-	// 返回 1 (TRUE) 表示我们已经处理了这个事件
-	// 这样 scum_run 就不会因为这些信号而退出
 	switch ctrlType {
 	case CTRL_C_EVENT, CTRL_BREAK_EVENT:
-		// 忽略这些事件，返回 TRUE
+		// 忽略 CTRL+C 和 CTRL+BREAK 事件
+		// 返回 1 (TRUE) 表示已处理，scum_run 不会退出
 		return 1
+
 	case CTRL_CLOSE_EVENT, CTRL_LOGOFF_EVENT, CTRL_SHUTDOWN_EVENT:
-		// 对于关闭、注销、关机事件，返回 FALSE 让系统处理
-		// 这样用户手动关闭控制台窗口时程序可以正常退出
+		// 控制台关闭、注销、关机事件
+		// 先调用关闭回调，让 SCUM 服务器优雅关闭
+		if shutdownCallback != nil {
+			shutdownCallback()
+			// 给 SCUM 服务器一些时间来保存数据
+			time.Sleep(5 * time.Second)
+		}
+		// 返回 0 让程序退出，或者直接调用 os.Exit
+		os.Exit(0)
 		return 0
 	}
 	return 0
@@ -47,7 +69,7 @@ var handlerCallback uintptr
 // DisableCtrlC 禁用 Ctrl+C 和 Ctrl+Break 信号处理
 // @description: 在程序启动时调用，注册自定义处理器来忽略 CTRL+C 和 CTRL+BREAK
 //
-//	防止 scum_run 因任何控制台信号而退出
+//	同时处理控制台关闭事件，确保 SCUM 服务器可以优雅关闭
 //
 // @return: error 错误信息
 func DisableCtrlC() error {
